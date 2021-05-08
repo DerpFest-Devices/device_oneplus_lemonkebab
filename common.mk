23d0325ffddddbb3ac40216a3d25cf9f41bd625f
Akash <akashniki@gmail.com>
2021-05-08 21:06:49 +0800

    sm8250-common: Remove init.qcom.factory
    This is useless for us

--- common.mk
+++ common.mk
@@ -203,7 +203,6 @@ PRODUCT_PACKAGES += \
     init.qcom.coex.sh \
     init.qcom.early_boot.sh \
     init.qcom.efs.sync.sh \
-    init.qcom.factory.rc \
     init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sdio.sh \
