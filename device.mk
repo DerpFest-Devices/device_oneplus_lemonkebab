bf87a5c56a66d2c690ebf0c3e4bb7b25646f372d
Blaster4385 <venkateshchaturvedi12@gmail.com>
2021-09-04 16:04:14 +0530

    lemonkebab: Decommonize oneplus common tree

--- device.mk
+++ device.mk
@@ -444,7 +444,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    device/oneplus/common
+    device/oneplus/lemonkebab
 
 # Telephony
 PRODUCT_PACKAGES += \
