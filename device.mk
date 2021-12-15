fc8e4229a078ed898f7f7178de2781a03f889b97
johnmart19 <johnivan19999@gmail.com>
2021-12-15 21:53:06 +0800

    lemonkebab: Add missing android.hardware.media.omx
    Fixes: android.hardware.media.omx@1.0-service: Service not available yet
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -381,6 +381,7 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-service \
     init.qti.media.sh \
     libOmxAacEnc \
     libOmxAmrEnc \
@@ -390,7 +391,8 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_omx.vendor
 
 # OPFeature
 PRODUCT_COPY_FILES += \
